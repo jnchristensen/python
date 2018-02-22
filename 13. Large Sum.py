457a7f20ac74ee7537dd94c8e91908a2bb0eed12
joshnc <jcawesome@gmail.com>
2018-02-21 22:03:52 -0700

    removed semicolons

--- 13. Large Sum.py
+++ 13. Large Sum.py
@@ -1,12 +1,12 @@
 #Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
-f = open("13. file", "r");
+f = open("13. file", "r")
 
-sum = 0;
+sum = 0
 
 for i in f:
-    sum += int(i);
+    sum += int(i)
 
-sum = str(sum);
+sum = str(sum)
 
-print(sum[:10]);
+print(sum[:10])
